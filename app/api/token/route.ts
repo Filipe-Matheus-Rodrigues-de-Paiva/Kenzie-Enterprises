ab51c93b4760920dc376e6017209c61bb39a18ad
filipe-matheus <filipepaivinha@gmail.com>
2023-09-20 10:15:10 -0300

    fix token route

---
+++ app/api/token/route.ts
@@ -0,0 +1,17 @@
+import { prisma } from '@/app/lib/prisma';
+import { getToken } from 'next-auth/jwt';
+import { NextResponse } from 'next/server';
+
+export async function GET(req: any) {
+  const token = await getToken({ req });
+
+  if (!token || !token.sub) {
+    throw new Error('Usuário não autenticado');
+  }
+
+  const loggedUser = await prisma.user.findUnique({
+    where: { id: token.sub },
+  });
+
+  return NextResponse.json(loggedUser, { status: 200 });
+}
